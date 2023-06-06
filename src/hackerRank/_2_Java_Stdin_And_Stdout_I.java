14e34c08969c6b757791d0d82cefba130bc506e1
Vitor Nascimento <vi_vcn@hotmail.com>
2023-06-06 11:32:57 -0300

    First commit

---
+++ src/hackerRank/_2_Java_Stdin_And_Stdout_I.java
@@ -0,0 +1,15 @@
+package hackerRank;
+import java.util.Scanner;
+public class _2_Java_Stdin_And_Stdout_I {
+	public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+     scan.close();
+    }
+}
---
+++ src/hackerRank/_5_Java_Output_Formatting.java
@@ -0,0 +1,28 @@
+package hackerRank;
+import java.util.Scanner;
+public class _5_Java_Output_Formatting {
+
+		public static void main(String[] args) {
+	        Scanner sc=new Scanner(System.in);
+	        String[] s = new String[3];
+	        int[] x = new int[3];
+	        
+	        for(int i=0;i<3;i++)
+	        {
+	            s[i]=sc.next();
+	            x[i]=sc.nextInt();
+	            
+	        }
+	        System.out.println("================================");
+	        for(int i=0;i<3;i++) {
+	        	
+	        	System.out.printf("%-14s %03d\n", s[i], x[i]);
+	        	
+	        }
+	        
+	        System.out.println("================================");
+
+	        sc.close();
+		}
+		
+}
