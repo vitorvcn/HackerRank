ae8ce287b24d8ad219f26bc15f48fdb2b009aef4
Vitor Nascimento <vi_vcn@hotmail.com>
2023-06-08 19:31:25 -0300

    Exercise 9

---
+++ src/hackerRank/_9_Java_end_of_file.java
@@ -0,0 +1,20 @@
+package hackerRank;
+
+import java.util.Scanner;
+
+public class _9_Java_end_of_file {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int count = 1;
+		
+		while(sc.hasNext()) {
+			System.out.println(count + " " + sc.nextLine());
+			count++;
+		}
+		
+	
+		
+		sc.close();
+	}
+}
