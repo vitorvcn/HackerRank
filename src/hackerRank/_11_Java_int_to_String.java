02d1c50ef384b543a8070a53d4d9b5e3bd794164
Vitor Nascimento <vi_vcn@hotmail.com>
2023-06-08 21:58:47 -0300

    Exercise 11

---
+++ src/hackerRank/_11_Java_int_to_String.java
@@ -0,0 +1,23 @@
+package hackerRank;
+
+
+import java.util.Scanner;
+
+public class _11_Java_int_to_String {
+
+	public static void main(String[] args) {
+
+		   Scanner in = new Scanner(System.in);
+		   int n = in .nextInt();
+		   in.close();
+		   //String s=???; Complete this line below
+		   
+		   String s = Integer.toString(n);
+		   System.out.println(s);
+		   
+	}
+}
+
+		   //Write your code here
+
+		 
